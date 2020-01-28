8a87db58a1d78754cb6461754c52bd6115a2d847
Esther Walker <equestriennesrule@yahoo.com>
2020-01-27 17:54:09 -0800

    added an html file and worked on node.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+
+    input: process.stdin,
+    output: process.stdout
+
+});
+
+rl.question('what is your name?', (answer) => {
+    console.log(`Nice to meet you: ${answer}`);
+    rl.close();
+});
+
+

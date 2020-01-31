407bceea22a4cde205e7d9e455d139b61dcd4bdd
Esther Walker <equestriennesrule@yahoo.com>
2020-01-30 17:02:21 -0800

    added axios! :D

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 'use strict'
 
 var inquirer = require('inquirer');
+const axios = require('axios');
 
-inquirer
-    .prompt([
-        {
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             message: 'What is your github user name?',
+//             name: 'username'
+//         },
+
+//         {
+//             type: 'list',
+//             message: 'What is your favorite color?',
+//             name: 'color',
+//             choices: ['orange', 'yellow', 'red', 'blue', 'green', 'purple', 'pink', 'black', 'white',]
+//         }
+//     ])
+//     .then(
+async function init() {
+    try {
+        const userInfo = await inquirer.prompt({
             type: 'input',
-            message: 'What is your github user name?',
+            message: 'what is your git hub username?',
             name: 'username'
-        },
+        });
+        const GH = await axios.get(`https://api.github.com/users/${userInfo.username}`)
+        console.log(GH.data);
+    } catch (err) {
+        console.error(err);
+    }
 
-        {
-            type: 'list',
-            message: 'What is your favorite color?',
-            name: 'color',
-            choices: ['orange', 'yellow', 'red', 'blue', 'green', 'purple', 'pink', 'black', 'white',]
-        }
-    ])
-    .then(function (response) {
-        console.log(response);
 
-    });
+};
+
+init()
+
+
+
+
 
 
 

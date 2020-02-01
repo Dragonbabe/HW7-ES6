cef796de146e61c214b26b008ba2979d9affbe88
Esther Walker <equestriennesrule@yahoo.com>
2020-02-01 08:41:03 -0800

    playing around with css; trying get visuals right

--- index.js
+++ index.js
@@ -3,31 +3,26 @@
 var inquirer = require('inquirer');
 const axios = require('axios');
 
-// inquirer
-//     .prompt([
-//         {
-//             type: 'input',
-//             message: 'What is your github user name?',
-//             name: 'username'
-//         },
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your github user name?',
+        name: 'username'
+    },
 
-//         {
-//             type: 'list',
-//             message: 'What is your favorite color?',
-//             name: 'color',
-//             choices: ['orange', 'yellow', 'red', 'blue', 'green', 'purple', 'pink', 'black', 'white',]
-//         }
-//     ])
-//     .then(
+    {
+        type: 'list',
+        message: 'What is your favorite color?',
+        name: 'color',
+        choices: ['orange', 'yellow', 'red', 'blue', 'green', 'purple', 'pink', 'black', 'white',]
+    }
+];
+
 async function init() {
     try {
-        const userInfo = await inquirer.prompt({
-            type: 'input',
-            message: 'what is your git hub username?',
-            name: 'username'
-        });
-        const GH = await axios.get(`https://api.github.com/users/${userInfo.username}`)
-        console.log(GH.data);
+        const userInfo = await inquirer.prompt(questions);
+        const gitHub = await axios.get(`https://api.github.com/users/${userInfo.username}`)
+        console.log(gitHub.data);
     } catch (err) {
         console.error(err);
     }
